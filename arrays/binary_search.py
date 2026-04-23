11d6bd0f7ae696969e46ce5fa0cb0ea2939c4db8
Kevin <console.logkevin@gmail.com>
2026-04-22 22:33:40 -0400

    feature(Big O): Big O Notation intro
    - how the running time increases as the list size increases. That’s
    where big O notation comes in.
    - “Big O notation lets you compare the number of operations. It tells
    you how fast the algorithm grows.”
    - The five most common algorithms are log time, linear time, factorial
    time, linearithmic time, quadratic time

--- arrays/binary_search.py
+++ arrays/binary_search.py
@@ -21,3 +21,36 @@
 
 # result = binary_search(arr_1, 56)
 # print(result)
+
+# 1.1 List of names
+'''
+Suppose you have a sorted list of 128 names, and you’re searching 
+through it using binary search. What’s the maximum number of steps 
+it would take?
+
+Answer: 7 because 2^7 equals 128 names
+'''
+
+# 1.2 Double the size
+'''
+“Suppose you double the size of the list. What’s the maximum number of steps now?”
+
+Answer: 8 steps max for double the size of the array
+'''
+
+# 1.3 Phone number in the phone book
+'''
+You have a name, and you want to find the person’s phone number in the 
+phone book.”
+
+Answer: O(log n)
+'''
+
+# 1.4
+'''
+“You have a phone number, and you want to find the person’s name in the 
+phone book. (Hint: You’ll have to search through the whole book!)”
+
+
+Answer: O(n)
+'''
